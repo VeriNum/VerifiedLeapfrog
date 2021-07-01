008de4cdb9b36ce78e21a82a66bb412da25479e0
ak2485 <ak2485@cornell.edu>
2021-07-01 11:52:01 -0400

    fix lemma names

--- coq/Euler.v
+++ coq/Euler.v
@@ -24,8 +24,8 @@ Fixpoint diff_y (n: nat) (t: R) (x: R) : R :=
   | S n => (Derive (diff_y n t) x) * t
   end.
 
-Fixpoint fcoe (n:nat) (x: R) : R := 
-  let t  := mod_eq fcoe n x in 
+Fixpoint fcoe_fix (n:nat) (x: R) : R := 
+  let t  := mod_eq fcoe_fix n x in 
   match n with
   | 0 => F x
   | (S n as m) => 
@@ -33,14 +33,23 @@ Fixpoint fcoe (n:nat) (x: R) : R :=
     (ytilde m) - x - h*(F x)
   end.
 
-Lemma foce2 :
-  fcoe 2 y = h^2 * y^3.
+Definition fcoe (n : nat) ( x : R) : R := -(fcoe_fix n x) .
+
+Lemma fcoe1 :
+  h > 0 -> fcoe 1 y = h * y^2.
 Proof.
-unfold fcoe, F; simpl. field_simplify. 
+intros. unfold fcoe, fcoe_fix, F; simpl. field_simplify. nra. 
+Qed. 
+
+Lemma fcoe2 :
+  fcoe 2 y = -h^2 * y^3.
+Proof.
+unfold fcoe, fcoe_fix, F; simpl. field_simplify. 
 replace (Derive (fun x : R => Derive (fun x0 : R => x0) x * (1 * (y * (y * 1)))) y) with (2*y). 
 replace (Derive (fun x : R => x) y) with 1. nra. 
 symmetry; field_simplify;
 apply is_derive_unique; auto_derive; auto; nra.
 symmetry; field_simplify. 
+
  
 
