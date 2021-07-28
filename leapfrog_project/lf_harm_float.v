15cd2c4a774680cfc475153af35afc7819662e7a
ak2485 <ak2485@cornell.edu>
2021-07-27 21:23:13 -0400

    updates to float ops

--- leapfrog_project/lf_harm_float.v
+++ leapfrog_project/lf_harm_float.v
@@ -1,63 +1,28 @@
-From Flocq Require Core Binary.
-Import Defs Raux Binary.
-
-Require Import Recdef.
-Require compcert.lib.Floats.
+From Flocq Require Import Binary Bits Core.
+From compcert.lib Require Import IEEE754_extra Coqlib Floats Zbits Integers.
 
 Section LeapfrogDefs.
 
-Definition float := Floats.float32.
-
-Definition binop_nan : forall x y : float, {x: float | is_nan _ _ x = true}
-    := Floats.Float32.binop_nan.
-
-(*Start Defs. attributed to Appel, Bertot https://github.com/cverified/cbench-vst/blob/master/sqrt/float_lemmas.v*)
-Definition ms :=  (* mantissa bits *)
-     ltac:(let t := eval compute in float in 
-            match t with binary_float ?m ?e => let x := constr:(m) in apply x end).
-Definition es :=  (* maximum exponent *)
-    ltac:(let t := eval compute in float in 
-            match t with binary_float ?m ?e => let x := constr:(e) in apply x end).
-
-Definition float_of_Z : Z -> float := 
-     IEEE754_extra.BofZ ms es eq_refl eq_refl.
-
-Definition float_plus: float -> float -> float :=
-   Bplus ms es eq_refl eq_refl binop_nan mode_NE.
-
-Definition float_mult: float -> float -> float :=
-   Bmult ms es eq_refl eq_refl binop_nan mode_NE.
-
-Definition float_div: float -> float -> float :=
-   Bdiv ms es eq_refl eq_refl binop_nan mode_NE.
-(*End Defs. attributed to Appel, Bertot*)
-
-Fixpoint float_pow (f : float) (n : nat) : float :=
-  match n with
-    | O => float_of_Z 1
-    | S n => float_mult f (float_pow f n)
-  end.
-
 (* Linear forcing function *)
-Definition F (x : float) : float := float_mult (float_of_Z (Z.opp 1)) x.
+Definition F (x : float32) : float32 := Float32.neg x.
 
 (* Time step*)
-Definition h := float_div (float_of_Z 1) (float_of_Z 32).
+Definition h := Float32.div (Float32.of_int (Int.repr 1%Z)) (Float32.of_int (Int.repr 32%Z)).
 
 (* Single step of the integrator*)
-Definition leapfrog_step ( ic : float * float) : float * float :=
+Definition leapfrog_step ( ic : float32 * float32) : float32 * float32 :=
   let x  := fst ic in let v:= snd ic in 
-  let x1 := float_plus x (float_mult h v) in 
-  let x2 := float_div (float_mult (float_pow h 2) (F x)) (float_of_Z 2) in
-  let x' := float_plus x1 x2 in
-  let v1 := float_plus (F x) (F x') in
-  let v' := float_plus v (float_div (float_mult h v1) (float_of_Z 2)) in 
+  let x1 := Float32.add x (Float32.mul h v) in 
+  let x2 := Float32.div (Float32.mul (Float32.mul h h) (F x)) (Float32.of_int (Int.repr 2%Z))  in
+  let x' := Float32.add x1 x2 in
+  let v1 := Float32.add (F x) (F x') in
+  let v' := Float32.add v (Float32.mul (Float32.mul h v1) (Float32.of_int (Int.repr 2%Z))) in 
   (x', v').
 
 (* Main *)
-Fixpoint leapfrog ( ic : float * float) (n : nat) : float * float:=
+Fixpoint leapfrog ( ic : float32 * float32) (n : nat) : float32 * float32:=
   match n with
-  | 0 => ic
+  | 0%nat => ic
   | S n' =>
     let  ic' := leapfrog_step ic in
     leapfrog ic' n'
