2017593aa8e529ee4e9b8d05b3b9d4f6118dc266
ak2485 <ak2485@cornell.edu>
2021-11-11 07:03:46 -0500

    change F to neg x for optimizer

--- leapfrog_project/lf_harm_float.v
+++ leapfrog_project/lf_harm_float.v
@@ -5,17 +5,15 @@ Require Import float_lib.
 Definition half := Float32.div 1 2.
 
 (* Linear forcing function *)
-Definition F (x : float32) : float32 := (- 1%F32 * x)%F32.
+Definition F (x : float32) : float32 := (-x)%F32.
 
 (* Time step*)
 Definition h := Float32.div 1 32.
 
-Opaque F h half.
-
 (* Single step of the integrator*)
 Definition leapfrog_step ( ic : float32 * float32) : float32 * float32 :=
   (let x  := fst ic in let v:= snd ic in 
-  let x' := (x + h * v) + (h * h * F x)*half in
+  let x' := (x + h * v) + (h * (h * F x))*half in
   let v' :=  v + (half*h*(F x + F x')) in 
   (x', v'))%F32.
 
