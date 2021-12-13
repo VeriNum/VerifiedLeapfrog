f3e10c3cd32960720492816d5ccadfb68207a38b
Ariel Kellison <arielkellison@Ariels-MacBook-Pro.local>
2021-12-13 05:45:25 -0500

    plotting files

---
+++ leapfrog_project/plotting/lfharm_diff.c
@@ -0,0 +1,113 @@
+/*
+ * LFHARM.C: program to integrate harmonic oscillator using leapfrog.
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* generate a random floating point number from min to max */
+double randfrom(double min, double max)
+{
+    double range = (max - min);
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
+
+/* one step in single prec */
+void lfstep32(float **x, float **v, float h, int size)
+{
+	float a;
+
+	for (int n = 0; n < size; n++)
+	{
+		a = -1.0f * (*x)[n];
+  	(*x)[n] = (*x)[n] + h * (*v)[n] + 0.5f * (h * h * -1.0f * (*x)[n]);		/* position step */
+  	(*v)[n] = (*v)[n] + 0.5f * (h * -1.0f * (*x)[n] + a);		/* velocity step */
+	}
+}
+
+/* one step in double prec */
+void lfstep64(double **x, double **v, double h, int size)
+{
+
+	double a;
+
+	for (int n = 0; n < size; n++)
+	{
+		a = -1.0 * (*x)[n];
+  	(*x)[n] = (*x)[n] + h * (*v)[n] + 0.5 * (h * h * -1.0 * (*x)[n]);		/* position step */
+  	(*v)[n] = (*v)[n] + 0.5 * (h * -1.0 * (*x)[n] + a);		/* velocity step */
+	}
+}
+
+/* main */
+int main(int argc, char *argv[])
+{
+	
+	int max  = atoi(argv[1]);
+	double hd = 1.0/32.0;
+	float hf  = (float) hd;
+	char filename1[25]= "lf_in_data_";
+	char filename2[25]= "lf_out_data_";
+
+	strncat(filename1, argv[2], 5);
+	strncat(filename2, argv[2], 5);
+	strncat(filename1, ".txt", 5);
+	strncat(filename2, ".txt", 5);
+
+	FILE *lf_in_data;
+	FILE *lf_out_data;
+
+	double *xdrands = calloc(max, sizeof(double));
+	float *xfrands  = calloc(max, sizeof(float));
+	double *vdrands = calloc(max, sizeof(double));
+	float *vfrands  = calloc(max, sizeof(float));
+
+	for (int n = 0; n < max; n++){
+		xdrands[n] = randfrom(-1.0, 1.0);
+		xfrands[n] = (float) xdrands[n];
+		vdrands[n] = randfrom(-1.0, 1.0);
+		vfrands[n] = (float) vdrands[n];
+	}
+
+	lf_in_data = fopen(filename1, "w");
+	for (int i = 0; i < max; i++) {
+			fprintf(lf_in_data,"%.17g\n", xdrands[i]);
+	}
+	for (int i = 0; i < max; i++) {
+			fprintf(lf_in_data,"%.17g\n", vdrands[i]);
+	}
+	fclose(lf_in_data);
+
+	lfstep64(&xdrands,&vdrands,hd,max);
+	lfstep32(&xfrands,&vfrands,hf,max);
+
+	double *abs_err_x  = calloc(max, sizeof(double));
+	double *abs_err_v  = calloc(max, sizeof(double));
+
+	for (int i = 0; i < max; i++) {
+			abs_err_x[i] = fabs(xdrands[i] - xfrands[i]);
+			abs_err_v[i] = fabs(vdrands[i] - vfrands[i]);
+	}
+
+	lf_out_data = fopen(filename2, "w");
+	for (int i = 0; i < max; i++) {
+	    fprintf(lf_out_data, "%.17g\n", abs_err_x[i]);
+	}
+	for (int i = 0; i < max; i++) {
+			fprintf(lf_out_data, "%.17g\n", abs_err_v[i]);
+	}
+	fclose(lf_out_data);
+
+	free(xfrands);
+	free(xdrands);
+	free(vfrands);
+	free(vdrands);
+	free(abs_err_x);
+	free(abs_err_v);
+
+  return 0 ;
+}
+
---
+++ leapfrog_project/plotting/plot_errors.jl
@@ -0,0 +1,144 @@
+using Plots
+using Plots.PlotMeasures
+using DelimitedFiles
+using Random, Distributions
+
+fpath = @__DIR__
+
+# formally proven bound for one step of LF integration (position)
+pbnd      = 4719104053608481 / 37778931862957161709568
+
+function plot_comps(file1, file2, file3, file4)
+    """
+    resource: https://docs.julialang.org/en/v1/stdlib/DelimitedFiles/
+    """
+
+    # data for gcc; read in as matrices
+
+    in_rands_gcc = readdlm(file1, Float64)
+    out_xv_gcc   = readdlm(file2, Float64)
+
+    # turn into vectors
+
+    vec_in_gcc    = [x::Float64 for x in in_rands_gcc]
+    vec_xv_gcc    = [x::Float64 for x in out_xv_gcc]
+    len           = convert(Int64,length(vec_xv_gcc)/2)
+    vec_in_x_gcc  = [x for x in vec_in_gcc[1:len]]
+    vec_in_v_gcc  = [v for v in vec_in_gcc[len+1:len*2]]
+    vec_x_gcc     = [x for x in out_xv_gcc[1:len]]
+    vec_v_gcc     = [v for v in out_xv_gcc[len+1:len*2]]
+
+    max_x_gcc     = maximum(vec_x_gcc)
+
+    # data for CompCert
+
+    in_rands_cc = readdlm(file3, Float64)
+    out_xv_cc   = readdlm(file4, Float64)
+
+    vec_in_cc    = [x::Float64 for x in in_rands_cc]
+    vec_xv_cc    = [x::Float64 for x in out_xv_cc]
+    len          = convert(Int64,length(vec_xv_cc)/2)
+    vec_in_x_cc  = [x for x in vec_in_cc[1:len]]
+    vec_in_v_cc  = [v for v in vec_in_cc[len+1:len*2]]
+    vec_x_cc     = [x for x in out_xv_cc[1:len]]
+    vec_v_cc     = [v for v in out_xv_cc[len+1:len*2]]
+
+    max_x_cc     = maximum(vec_x_cc)
+
+    # plots for gcc
+
+    plot([pbnd for i in 1:len],seriestype="vline",label="formally proven bound=$pbnd" )
+    plot!([max_x_gcc for i in 1:len],seriestype="vline",label="worst empirical error=$max_x_gcc" )
+    plota = histogram!(vec_x_gcc, xlabel = "floating-point error",ylabel = "frequency",
+    label="empirical error",size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
+    legendfontsize=12, yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12, ytickfontsize = 12)
+    savefig(plota,fpath*"/gcc_errors.png")
+
+    plot([pbnd for i in -1:1],seriestype="hline",label="formally proven bound=$pbnd" )
+    plotb = scatter!(vec_in_x_gcc,vec_x_gcc, ylabel = "floating-point error",xlabel = "position val",
+    size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
+    yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12, ytickfontsize = 12, label="empirical error",
+    xlims = (-1,1))
+    savefig(plotb,fpath*"/gcc_errors2.png")
+
+    # plots for CompCert
+
+    plot([pbnd for i in 1:len],seriestype="vline",label="formally proven bound=$pbnd" )
+    plot!([max_x_cc for i in 1:len],seriestype="vline",label="worst empirical error=$max_x_cc" )
+    plota = histogram!(vec_x_cc, xlabel = "floating-point error",ylabel = "frequency",
+    label="empirical error",size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
+    legendfontsize=12, yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12, ytickfontsize = 12)
+    savefig(plota,fpath*"/cc_errors.png")
+
+    plot([pbnd for i in -1:1],seriestype="hline",label="formally proven bound=$pbnd" )
+    plotb = scatter!(vec_in_x_cc,vec_x_cc, ylabel = "floating-point error",xlabel = "position val",
+    size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
+    yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12, ytickfontsize = 12, label="empirical error",
+    xlims = (-1,1))
+    savefig(plotb,fpath*"/cc_errors2.png")
+end
+
+function leapfrog32(x::Float32,v::Float32,n::Int32,h::Float32)::Tuple{Float32,Float32}
+    for i in 1:n
+        xnew         = (x + h * v) + (h * h * - x) / 2f0
+        vnew         = v + (h*(- x + - xnew)/2f0)
+        x            = xnew
+        v            = vnew
+    end
+    return (x,v)
+end
+
+function leapfrog64(x::Float64,v::Float64,n::Int64,h::Float64)::Tuple{Float64,Float64}
+    for i in 1:n
+        xnew         = (x + h * v) + (h * h * - x) / 2
+        vnew         = v + (h*(- x + - xnew)/2)
+        x            = xnew
+        v            = vnew
+    end
+    return (x,v)
+end
+
+function plot_julia(sz)
+    # choose random starting values in interval
+    x1 = [Float64(rand(Uniform(-1,1))) for n in 1:sz]
+    v1 = [Float64(rand(Uniform(-1,1))) for n in 1:sz]
+    x2 = [Float32(x) for x in x1]
+    v2 = [Float32(v) for v in v1]
+    # define time step
+    h32 = (1f0/32f0)
+    h64 = 1/32
+    # integrate
+    LF32x = [leapfrog32(x2[i],v2[i],Int32(1),h32)[1] for i in 1:sz]
+    LF64x = [leapfrog64(x1[i],v1[i],1,h64)[1] for i in 1:sz]
+    # determine error
+    xs   = [abs(LF32x[i] - LF64x[i]) for i in 1:sz]
+    max1 = maximum(xs)
+
+    plot([pbnd for i in 1:sz],seriestype="vline",label="formally proven bound=$pbnd" )
+    plot!([max1 for i in 1:sz],seriestype="vline",label="worst empirical error=$max1" )
+    plota = histogram!(xs, xlabel = "floating-point error",ylabel = "frequency",
+    label="empirical error",size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
+    legendfontsize=12, yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12, ytickfontsize = 12)
+    savefig(plota,fpath*"/julia_errors.png")
+
+    plot([pbnd for i in -1:1],seriestype="hline",label="formally proven bound=$pbnd" )
+    plotb = scatter!(x1,xs, ylabel = "floating-point error",xlabel = "position val",
+    size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
+    yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12, ytickfontsize = 12, label="empirical error",
+    xlims = (-1,1))
+    savefig(plotb,fpath*"/julia_errors2.png")
+end
+
+function main(file1,file2,file3,file4,sz)
+    """file1 should be a txt file containing 2n
+    random inputs (n for position followed by n for velocity)
+
+    file2 should be a txt file containing 2n
+    outputs of m steps of integration
+    (n for position followed by n for velocity)
+    """
+
+    plot_comps(file1,file2,file3,file4)
+    plot_julia(sz)
+    
+end
