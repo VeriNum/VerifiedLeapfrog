4b3f2e40266d9c3dc2137f8a964d163cd3942896
ak2485 <ak2485@cornell.edu>
2021-12-28 21:22:16 -0500

    plotting updates

--- leapfrog_project/plotting/plot_errors.jl
+++ leapfrog_project/plotting/plot_errors.jl
@@ -4,21 +4,20 @@ using DelimitedFiles
 using Random, Distributions
 
 """
-Call this as jl script on any function in this file.
+Call as a jl script on any function in this file.
 e.g. julia -L plot_errors.jl -e 'main(args)'.
-
-Out: six plots, two for each compiler (gcc,
-compcert) and two for julia data.
 
 Author: Ariel Kellison, 12/2021
 """
 
 fpath = @__DIR__
 
-# formally proven bound for one step of LF integration (position)
-pbnd      = 4719104053608481 / 37778931862957161709568
 
-function plot_comps(in_file::String, err_file::String)
+# formally proven bounds 
+pbnd_x = 4719104053608481 / 37778931862957161709568 
+pbnd_v = 4934642575282197 / 75557863725914323419136
+
+function plot_errors(in_file::String, err_file::String, pos::Bool)
     """
     Args: (1) in_file contains the random inputs for integration
           (2) err_file contains the errors
@@ -29,12 +28,12 @@ function plot_comps(in_file::String, err_file::String)
     
     resource: https://docs.julialang.org/en/v1/stdlib/DelimitedFiles/
     """
-    # read in data, as matrices, from files
+    # read data as matrices from files
 
     in_rands = readdlm(in_file, Float64)
     out_xv   = readdlm(err_file, Float64)
 
-    # turn into vectors
+    # data to vec
 
     vec_in   = [x::Float64 for x in in_rands]
     vec_xv   = [x::Float64 for x in out_xv]
@@ -45,20 +44,27 @@ function plot_comps(in_file::String, err_file::String)
     vec_v    = [v for v in out_xv[len+1:len*2]]
 
     max_x    = maximum(vec_x)
-    
+
+    # plot position or velocity
+
+    pos ? fstr      = "position" : fstr      = "velocity"
+    pos ? pbnd      = pbnd_x     : pbnd      = pbnd_v
+    pos ? out_dat   = vec_x      : out_dat   = vec_v
+    pos ? in_dat    = vec_in_x   : in_dat    = vec_out_v
+
     # plot histogram 
 
     plot([pbnd for i in 1:len],seriestype="vline",label="formally proven bound=$pbnd",
 	left_margin = 15mm, right_margin = 15mm
 	 )
     plota = histogram!(
-        vec_x, xlabel = "floating-point error",ylabel = "frequency",
+        out_dat, xlabel = "floating-point error",ylabel = "frequency",
         label="empirical error",size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
         legendfontsize=12, yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12,
         ytickfontsize = 12,
-        title = "Leapfrog integration: absolute position error"
+        title = "Leapfrog integration: absolute $fstr error"
     )
-    savefig(plota,fpath*"/histo_errors.png")
+    savefig(plota,fpath*"/histo_errors_$fstr.png")
     
     # plot scatter
 
@@ -66,77 +72,11 @@ function plot_comps(in_file::String, err_file::String)
         left_margin = 15mm, right_margin = 15mm
 	 )
     plotb = scatter!(
-        vec_in_x, vec_x, ylabel = "floating-point error",xlabel = "initial position",
+        in_dat, out_dat, ylabel = "floating-point error",xlabel = "initial $plot_l",
         size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
         yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12, ytickfontsize = 12,
         label="empirical error",
-        xlims = (-1,1), title = "Leapfrog integration: absolute position error"    
+        xlims = (-1,1), title = "Leapfrog integration: absolute $fstr error"    
 	)
-    savefig(plotb,fpath*"/scatter_errors.png")
-end
-
-function leapfrog32(x::Float32,v::Float32,n::Int32,h::Float32)::Tuple{Float32,Float32}
-    for i in 1:n
-        xnew         = (x + h * v) + (h * h * - x) / 2f0
-        vnew         = v + (h*(- x + - xnew)/2f0)
-        x            = xnew
-        v            = vnew
-    end
-    return (x,v)
-end
-
-function leapfrog64(x::Float32,v::Float32,n::Int32,h::Float32)::Tuple{Float64,Float64}
-    for i in 1:n
-        xnew         = (x + h * v) + (h * h * - x) / 2
-        vnew         = v + (h*(- x + - xnew)/2)
-        x            = xnew
-        v            = vnew
-    end
-    return (x,v)
-end
-
-function plot_julia(sz)
-    # choose random starting values in interval
-    x2 = [Float64(rand(Uniform(-1,1))) for n in 1:sz]
-    v2 = [Float64(rand(Uniform(-1,1))) for n in 1:sz]
-    x1 = x2
-    v1 = v2
-    # define time step
-    h = (1f0/32f0)
-    # integrate
-    LF32x = [leapfrog32(x2[i],v2[i],1f0,h)[1] for i in 1:sz]
-    LF64x = [leapfrog64(x1[i],v1[i],1f0,h)[1] for i in 1:sz]
-    # determine error
-    xs   = [abs(LF32x[i] - LF64x[i]) for i in 1:sz]
-
-    plot([pbnd for i in 1:sz],seriestype="vline",label="formally proven bound=$pbnd" )
-    plota = histogram!(
-        xs, xlabel = "floating-point error",ylabel = "frequency",
-        label="empirical error",size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
-        legendfontsize=12, yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12,
-        ytickfontsize = 12, title = "Absolute position error - Julia"
-    )
-    savefig(plota,fpath*"/julia_errors.png")
-
-    plot([pbnd for i in -1:1],seriestype="hline",label="formally proven bound=$pbnd" )
-    plotb = scatter!(
-        x1,xs, ylabel = "floating-point error",xlabel = "position val",
-        size = (900, 500),bottom_margin = 10mm,left_margin = 10mm,
-        yguidefontsize=12, xguidefontsize=12, xtickfontsize = 12, ytickfontsize = 12,
-        label="empirical error",
-        xlims = (-1,1), title = "Absolute position error - Julia"
-    )
-    savefig(plotb,fpath*"/julia_errors2.png")
-end
-
-function main(file1,file2,sz)
-    """
-    Args: (1) in_file contains the random inputs for integration
-          (2) err_file contains the errors
-          (3) number of inputs for Julia plots
-    """
-
-    plot_comps(file1,file2)
-    plot_julia(sz)
-
+    savefig(plotb,fpath*"/scatter_errors_$fstr.png")
 end
