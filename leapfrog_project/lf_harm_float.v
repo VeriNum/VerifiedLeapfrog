3bd170627f4f824b50412c7f656a1fee9e5eed0e
ak2485 <ak2485@cornell.edu>
2022-03-31 09:19:33 -0400

    clean up floating point functional models

--- leapfrog_project/lf_harm_float.v
+++ leapfrog_project/lf_harm_float.v
@@ -12,71 +12,66 @@ Local Open Scope float32_scope.
 Section WITHNANS.
 Context {NANS: Nans}.
 
+(** A single step of integration can modeled as either 
+(1) a transition matrix update to the vector (p,q) of momentum "p" and position "q", as
+  given in "leapfrog_stepF"
+-or-
+(2) the "typical" velocity verlet scheme, as given in "leapfrog_stepF_ver" **)
+
+
+(** Compute one time-step: given "ic" which is a pair of momentum "q" and position "p" ,
+  calculate the new position and mometum after time "h" has elapsed. **)
+
+Definition h := (1 / 32)%F32.   (* Time-step: 1/32 of a second *)
+
+
+(* Single step of integration modeling a transition matrix update *)
+Definition leapfrog_stepF (ic : ftype Tsingle * ftype Tsingle) : (ftype Tsingle * ftype Tsingle)  :=
+  let p:= fst ic in let q:= snd ic in 
+  let q' := (1 - ((1/2) * (h * h))) * q + (h * p) in
+  let p' := (1 - ((1/2) * (h * h))) * p - ((1/2 * h) * (2 - ((1/2) * (h * h)))) * q  in 
+  (p', q').
+
+
+
 (* Linear forcing function *)
 Definition F (x : ftype Tsingle) : ftype Tsingle := -x.
 
-(* Time step*)
-Definition h := 1 / 32.
-Definition half_h := 1 / 64.
-Definition pow2_h := 1/1024.
-Definition half_pow2_h := 1/2048.
 
 
-(* Single step of integration*)
-Definition leapfrog_stepF' ( ic : ftype Tsingle * ftype Tsingle) : ftype Tsingle * ftype Tsingle :=
-  let x  := fst ic in let v:= snd ic in 
-  let x' := x + h * v + half_pow2_h * F x in
-  let v' :=  v +  half_h * (F x + F x')  in 
-  (x', v').
-
-
-(* Single step of integration*)
-Definition leapfrog_stepF'' ( ic : ftype Tsingle * ftype Tsingle) : ftype Tsingle * ftype Tsingle :=
+(* Single step of Verlet integration *)
+Definition leapfrog_stepF_ver (ic : ftype Tsingle * ftype Tsingle) : ftype Tsingle * ftype Tsingle :=
   let x  := fst ic in let v:= snd ic in 
   let x' := (x + h * v) + ((1/2) * (h * h)) * F x in
   let v' :=  v +  (1/2 * h) * (F x + F x') in 
   (x', v').
 
 
-Lemma lf_funs_eq ( ic : ftype Tsingle * ftype Tsingle):
-leapfrog_stepF' ic = leapfrog_stepF'' ic.
-Proof.
-unfold leapfrog_stepF', leapfrog_stepF'', F, half_pow2_h, h, half_h.
-replace (1 / 2048) with (1 / 2 * (1 / 32 * (1 / 32))); auto.
-replace (1 / 64) with (1 / 2 * (1 / 32)); auto.
-all: apply B2R_inj; auto.
-Qed.
-
+(** Iterations **)
 
 (* Main *)
-Fixpoint leapfrogF' ( ic : ftype Tsingle * ftype Tsingle) (n : nat) : ftype Tsingle * ftype Tsingle:=
+Fixpoint leapfrogF (Step: (ftype Tsingle * ftype Tsingle) -> (ftype Tsingle * ftype Tsingle)) 
+  (ic: ftype Tsingle * ftype Tsingle) (n : nat): ftype Tsingle * ftype Tsingle:=
   match n with
   | 0%nat => ic
   | S n' =>
-    let  ic' := leapfrog_stepF' ic in
-    leapfrogF' ic' n'
-  end.
-
-Definition leapfrog_stepF ic : ftype Tsingle * ftype Tsingle :=
-  let p:= fst ic in let q:= snd ic in 
-  let q' := (1 - half_pow2_h) * q + (h * p) in
-  let p' := (1 - half_pow2_h) * p - (half_h * (2 - half_pow2_h)) * q  in 
-  (p', q').
-
-(* assumes inputs of (p, w * q, w, n) *)
-(* output q' will therefore be scaled appropriately *)
-Fixpoint leapfrogF (ic: ftype Tsingle * ftype Tsingle) (n : nat): ftype Tsingle * ftype Tsingle:=
-  match n with
-  | 0%nat => ic
-  | S n' =>
-    let ic' :=  leapfrog_stepF ic in
-  leapfrogF ic' n'
+    let ic' :=  Step ic in
+  leapfrogF Step ic' n'
 end.
+
+
+Definition iternF (ic: ftype Tsingle * ftype Tsingle) (n:nat) :=  leapfrogF leapfrog_stepF ic n.
+
+Definition iternF_ver (ic: ftype Tsingle * ftype Tsingle) (n:nat) :=  leapfrogF leapfrog_stepF_ver ic n.
+
+
+
+(** Lemmas **)
 
 
 Lemma lfstep_lfn:
   forall n ic ,
-  leapfrog_stepF (leapfrogF ic n) = leapfrogF (leapfrog_stepF ic) n.
+  leapfrog_stepF (leapfrogF leapfrog_stepF ic n) = leapfrogF leapfrog_stepF (leapfrog_stepF ic) n.
 Proof.
 induction n. 
 - auto.
@@ -85,25 +80,22 @@ Qed.
 
 Lemma lfn_eq_lfstep:
   forall n ic ,
-  leapfrogF ic (S n) = leapfrog_stepF (leapfrogF ic n).
+  leapfrogF leapfrog_stepF ic (S n) = leapfrog_stepF (leapfrogF leapfrog_stepF ic n).
 Proof.
 induction n.
 - auto.
 - intros. rewrite -> IHn. simpl. 
-replace (leapfrog_stepF (leapfrogF ic n)) with (leapfrogF (leapfrog_stepF ic) n). destruct (leapfrog_stepF ic). 
+replace (leapfrog_stepF (leapfrogF _ _ _ )) with (leapfrogF leapfrog_stepF (leapfrog_stepF ic) n). 
+  destruct (leapfrog_stepF ic). 
 all: symmetry; apply lfstep_lfn. 
 Qed.
-
-
-Definition iternF (ic: ftype Tsingle * ftype Tsingle) (n:nat) :=  leapfrogF ic n.
-
 
 Lemma step_iternF : 
   forall n : nat,
   forall ic, 
-  (iternF ic (S n)) = leapfrog_stepF (iternF ic n ).
+  (iternF  ic (S n)) = leapfrog_stepF (iternF ic n ).
 Proof.
-intros; unfold iternF; 
+intros; unfold iternF.
 rewrite ?lfn_eq_lfstep; 
 congruence.
 Qed.
